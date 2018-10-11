7ce2afbbab9e08bcc3bde4a8d14972adbd447c69
HaysS <stanfordhays@gmail.com>
2018-10-11 13:11:41 -0500

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+import Layout from '../components/layout'
+
+const IndexPage = () => (
+  <Layout>
+    <h1>BEHOLD.</h1>
+    <p>BEHOLD. Hays Stanford has granted you his presence to his glory.</p>
+    <p>BASK IN THE WONDER BEFORE US ALL.</p>
+    <Link to="/page-2/">Transport to an entirely different dimension.</Link>
+  </Layout>
+)
+
+export default IndexPage
